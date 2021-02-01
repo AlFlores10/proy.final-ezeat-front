fbbd2c647fa6d8cc290ca1650005b784f533c3ff
AlFlores10 <alberto.flores.1990@gmail.com>
2021-02-01 06:02:02 +0100

    Finish Feature Containers/Contact.jsx & Contact.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 
+//Containers
 import Header from './containers/Header/Header';
 import Footer from './containers/Footer/Footer';
 import Register from './containers/Register/Register';
 import About from './containers/About/About';
+import Contact from './containers/Contact/Contact';
 
+//Components
 import Home from './components/Home/Home';
 import RegisterCustomer from './components/RegisterCustomer/RegisterCustomer';
 import RegisterRestaurant from './components/RegisterRestaurant/RegisterRestaurant';
@@ -24,6 +27,8 @@ function App() {
           <Route path="/restaurant/signup" exact component={RegisterRestaurant} />
           <Route path="/login" exact component={Login} />
           <Route path="/about" exact component={About} />
+          <Route path="/contact" exact component={Contact} />
+
 
         </Switch>
         <Footer />
